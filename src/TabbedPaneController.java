14d39777c1a70a2651992c518e95ce44cb6f3a1c
Giacobbi <agiacobbi@zagmail.gonzaga.edu>
2019-12-12 14:29:37 -0800

    take survey bug- opens when none selected

--- src/TabbedPaneController.java
+++ src/TabbedPaneController.java
@@ -118,6 +118,10 @@ public class TabbedPaneController {
              */
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                if (view.surveysList.isSelectionEmpty()) {
+                    return;
+                }
+
                 String selectedSurvey = view.surveysList.getSelectedValue();
                 SurveyDatabaseHelper getHelper = new SurveyDatabaseHelper(selectedSurvey);
                 Survey s = getHelper.getSurvey();
@@ -135,6 +139,10 @@ public class TabbedPaneController {
              */
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                if (view.availableResultsList.isSelectionEmpty()) {
+                    return;
+                }
+
                 String selectedSurvey = view.availableResultsList.getSelectedValue();
                 new SurveyResultController(selectedSurvey);
             }
